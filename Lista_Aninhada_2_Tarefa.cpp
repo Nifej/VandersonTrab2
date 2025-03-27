db5fcdcd188530f9fe4072f53489003d9145b6b2
Jefferson De Jesus Da Silva <2024122760237@cefetes.br>
2025-03-27 13:19:19 -0300

    Primeiro commit

---
+++ Lista_Aninhada_2_Tarefa.cpp
@@ -0,0 +1,88 @@
+/* Lista Aninhada 2 
+   26-03-2025
+   Tarefa Avaliativa
+   
+   Trabalho em Dupla:
+   
+   (1).....................................................
+   (2).....................................................
+   
+*/
+
+typedef char string[40];
+
+typedef struct tipoDisciplina {
+	string nome;
+	int cargaHoraria;
+	tipoDisciplina *prox;
+	TPreRequisito *preRequisitos;	
+}TDisciplina;
+
+typedef struct tipoPreRequisito {
+	TDisciplina *preRequisito;
+	tipoPreRequisito *prox;
+}TPreRequisito;
+
+typedef struct tipoGrade {
+	TDisciplina *disciplina;
+	int periodo;
+	tipoGrade *prox;	
+}TGrade;
+
+typedef struct tipoCurso {
+	string nome;
+	tipoCurso *ante;
+	tipoCurso *prox;
+	TGrade *gradeCurricular;
+}TCurso;
+
+typedef struct tipoLista {
+	TCurso *cursos;
+	TDisciplina *disciplinas;
+}TLista;
+
+TLista lista;
+
+void inicializa(TLista *L);
+
+int main(){
+	
+}
+//===| Funções |==================================================
+void inicializa(TLista *L){
+	L->cursos = NULL;
+	L->disciplinas = NULL;	
+}
+//================================================================
+int menu(){
+	int opcao;
+	system("CLS");
+	printf("\n\n\n\t\t=======| MENU |=======\n\n");
+	printf("\t0 - SAIR (Encerrar Aplicacao).\n\n");
+	printf("\t1 - Inserir DISCIPLINAS.\n");
+	printf("\t2 - Excluir DISCIPLINAS.\n\n");
+	printf("\t3 - Inserir CURSOS.\n");
+	printf("\t4 - Excluir CURSOS.\n");
+	printf("\t5 - Registrar PRE-REQUISITOS para DISCIPLINA.\n");
+	printf("\t6 - Excluir PRE-REQUISITO de DISCIPLINA.\n\n");
+	printf("\t7 - Registrar GRADE CURRICULAR para CURSO.\n");
+	printf("\t8 - Excluir item da GRADE CURRICULAR de CURSO.\n\n");
+	printf("\t9 - Listar DISCIPLINAS.\n");
+	printf("\t10- Listar CURSOS.\n");
+	printf("\t11- Listar PRE_REQUISITOS de DISCIPLINA especifica.\n");
+	printf("\t12- Listar GRADE CURRICULAR de CURSO especifico.\n\n");
+	
+	printf("\t\tInforme a OPCAO desejada: ");
+	scanf("%d", &opcao);
+	
+	if((opcao < 0) || (opcao > 12)){
+		printf("\n\n\n\tERRO: OPCAO invalida!\n\tTente Novamente\n\n");
+		system("PAUSE");
+	}//if
+	return opcao;
+}
+//================================================================
+
+
+
+
